85647ed3cd152a4f373594a0dd4abadd60c063b6
TiHau <tim.hauenstein97@gmx.de>
2019-04-18 17:21:35 +0200

    added auto filename generation

--- downloader.py
+++ downloader.py
@@ -1,17 +1,37 @@
+import datetime
 import ftplib
 
 
 def download():
-    ftp = ftplib.FTP('ftp.ncep.noaa.gov')
+    now = datetime.datetime.now()
 
-    ftp.login()
+    if now.hour < 9:
+        hour = '00'
+    elif now.hour < 13:
+        hour = '08'
+    elif now.hour < 19:
+        hour = '12'
+    else:
+        hour = '18'
 
-    ftp.cwd('/pub/data/nccf/com/gfs/prod/gfs.2019040812/')
+    file = 'gfs.t' + hour + 'z.pgrb2.0p25.f003'
+    path = '/pub/data/nccf/com/gfs/prod/gfs.' + str(now.year) + str(now.month) + str(now.day) + hour + '/'
 
-    file = "gfs.t12z.pgrb2.0p25.f003"  # example filename
-
-    ftp.retrbinary('RETR ' + file, open(file, 'wb').write, 1024)
-
-    ftp.quit()
+    # only download if file doesn't exist
+    try:
+        open(file, 'r')
+        print('file exists')
+    except FileNotFoundError:
+        print('Starting download')
+        ftp = ftplib.FTP('ftp.ncep.noaa.gov')
+        ftp.login()
+        ftp.cwd(path)
+        ftp.retrbinary('RETR ' + file, open(file, 'wb').write, 1024)
+        ftp.quit()
+        print('downloaded: ' + file)
 
     return file
+
+
+if __name__ == '__main__':
+    download()
