988f3fdbe68a1244099c9d391610c555057aec66
jasafasa <f.steinhauser@hotmail.de>
2019-04-18 11:03:40 +0200

    write data to csv from src grib2

--- extract-data.py
+++ extract-data.py
@@ -9,19 +9,20 @@ def test():
     coordinate2 = (48.137154, 11.576124)
 
     f = grib.open(file)
+    for lines in f:
+        tmp = str(lines).split(':')[:2]
+        if "wind" in tmp[1].lower():
+            print(tmp)
 
-    print(f[5])
-    #for g in f:
-    g=f[5]
-       # print(g)
-    csv_name = str(g.typeOfLevel) + "-" + str(g.level) + "-" + str(g.name) + "-data-" + str(g.validDate) + "-" + str( g.analDate) + "-" + str(g.forecastTime)
-    import csv
+            g = f[int(tmp[0])]
+            csv_name = str(g.typeOfLevel) + "-" + str(g.level) + "-" + str(g.name) + "-data-" + str(g.validDate) + "-" + str( g.analDate) + "-" + str(g.forecastTime)
+            import csv
 
-    with open(csv_name + ".csv", "w+") as my_csv:
-        csvWriter = csv.writer(my_csv, delimiter=',')
-        for k in g.data(coordinate1[0], coordinate2[0], coordinate1[1], coordinate2[1]):
-            for l in k:
-                csvWriter.writerow(l)
+            with open(csv_name + ".csv", "w+") as my_csv:
+                csvWriter = csv.writer(my_csv, delimiter=',')
+                for k in g.data(coordinate1[0], coordinate2[0], coordinate1[1], coordinate2[1]):
+                    for l in k:
+                        csvWriter.writerow(l)
 
 
 
