7020f096619db28f88103762974fd36a25eb8208
jasafasa <f.steinhauser@hotmail.de>
2019-04-18 10:47:48 +0200

    readable csv on 1 data set

--- extract-data.py
+++ extract-data.py
@@ -5,8 +5,8 @@ import numpy
 
 def test():
     file = 'gfs.t00z.pgrb2.0p25.f003'  # example filename
-    coordinate1=(47.66033, 9.17582)
-    coordinate2=(48.137154, 11.576124)
+    coordinate1 = (47.66033, 9.17582)
+    coordinate2 = (48.137154, 11.576124)
 
     f = grib.open(file)
 
@@ -14,13 +14,16 @@ def test():
     #for g in f:
     g=f[5]
        # print(g)
-    print(g.typeOfLevel, g.level, g.name, g.data(10,20,30,40), g.validDate, g.analDate, g.forecastTime)
+    csv_name = str(g.typeOfLevel) + "-" + str(g.level) + "-" + str(g.name) + "-data-" + str(g.validDate) + "-" + str( g.analDate) + "-" + str(g.forecastTime)
     import csv
 
-
-    with open("new_file.csv", "w+") as my_csv:
+    with open(csv_name + ".csv", "w+") as my_csv:
         csvWriter = csv.writer(my_csv, delimiter=',')
-        csvWriter.writerows(g.data(47.66033,48.137154,9.17582,11.576124))
+        for k in g.data(coordinate1[0], coordinate2[0], coordinate1[1], coordinate2[1]):
+            for l in k:
+                csvWriter.writerow(l)
+
+
 
 if __name__ == '__main__':
     test()
