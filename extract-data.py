dfd5eb0d9c5b498dd5b014c303148833e7cc1964
jasafasa <f.steinhauser@hotmail.de>
2019-04-20 10:28:42 +0200

    added pressure and height

--- extract-data.py
+++ extract-data.py
@@ -1,6 +1,7 @@
 import pygrib as grib
 import struct
 import numpy
+import csv
 
 
 def test():
@@ -9,14 +10,41 @@ def test():
     coordinate2 = (48.137154, 11.576124)
 
     f = grib.open(file)
+    #for g in f:
+     #   print(g)
     for lines in f:
         tmp = str(lines).split(':')[:2]
         if "wind" in tmp[1].lower():
             print(tmp)
-
             g = f[int(tmp[0])]
             csv_name = str(g.typeOfLevel) + "-" + str(g.level) + "-" + str(g.name) + "-data-" + str(g.validDate) + "-" + str( g.analDate) + "-" + str(g.forecastTime)
-            import csv
+
+
+            with open(csv_name + ".csv", "w+") as my_csv:
+                csvWriter = csv.writer(my_csv, delimiter=',')
+                for k in g.data(coordinate1[0], coordinate2[0], coordinate1[1], coordinate2[1]):
+                    for l in k:
+                        csvWriter.writerow(l)
+        if "pressure" in tmp[1].lower():
+            print(tmp)
+
+            g = f[int(tmp[0])]
+            csv_name = str(g.typeOfLevel) + "-" + str(g.level) + "-" + str(g.name) + "-data-" + str(
+                g.validDate) + "-" + str(g.analDate) + "-" + str(g.forecastTime)
+
+
+            with open(csv_name + ".csv", "w+") as my_csv:
+                csvWriter = csv.writer(my_csv, delimiter=',')
+                for k in g.data(coordinate1[0], coordinate2[0], coordinate1[1], coordinate2[1]):
+                    for l in k:
+                        csvWriter.writerow(l)
+        if "height" in tmp[1].lower():
+            print(tmp)
+
+            g = f[int(tmp[0])]
+            csv_name = str(g.typeOfLevel) + "-" + str(g.level) + "-" + str(g.name) + "-data-" + str(
+                g.validDate) + "-" + str(g.analDate) + "-" + str(g.forecastTime)
+
 
             with open(csv_name + ".csv", "w+") as my_csv:
                 csvWriter = csv.writer(my_csv, delimiter=',')
