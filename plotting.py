6d5a20c6a208163d1ddfc253e0a5b51d778cd125
ckwiatkowski <corina.kwiatkowski@buchter.com>
2019-06-14 13:44:09 +0200

    separated interpolating from plotting

--- plotting.py
+++ plotting.py
@@ -7,23 +7,10 @@ import datetime as dt
 import util
 import time
 
-# arrival_time: time of arrival as last value on x-axis
-# height: highest displayed point above sea level
-def plotting(num_waypoints, flight_data, grib_data):
-
-    #exception handling
-    if num_waypoints == 0:
-        print("Number of waypoints must be higher than 0")
-        exit(1)
-
-    # constants
-    FEET_TO_METERS = 0.305
-    UPPER_LIMIT = 1000
-    LOWER_LIMIT = 50
-
-    # getti
+def interpolating(flight_data, grib_data, num_waypoints):
     res = []
     at_waypoint = []
+
     for entry in flight_data.get_path_filtered(num_waypoints):
         tl_lat = util.round_to_nearest_quarter_up(entry.latitude)
         tl_long = util.round_to_nearest_quarter_down(entry.longitude)
@@ -52,33 +39,53 @@ def plotting(num_waypoints, flight_data, grib_data):
                 except:
                     pass
         res.append(res_values)
+    return res, at_waypoint
 
-        # putting data in lists
-        heights = {}
-        u_comp = {}
-        v_comp = {}
-        temperature = {}
+# arrival_time: time of arrival as last value on x-axis
+# height: highest displayed point above sea level
+def plotting(num_waypoints, res, at_waypoint):
 
-        for en in res:
-            for val in en:
-                if LOWER_LIMIT <= val[0] <= UPPER_LIMIT and val[1] == 'isobaricInhPa':
-                    if val[2] == 'Geopotential Height':
-                        if val[0] not in heights:
-                            heights[val[0]] = []
-                        heights[val[0]].append(val[4])
-                    elif val[2] == 'U component of wind':
-                        if val[0] not in u_comp:
-                            u_comp[val[0]] = []
-                        u_comp[val[0]].append(val[4])
-                    elif val[2] == 'V component of wind':
-                        if val[0] not in v_comp:
-                            v_comp[val[0]] = []
-                        v_comp[val[0]].append(val[4])
-                    elif val[2] == 'Temperature':
-                        if val[0] not in temperature:
-                            temperature[val[0]] = []
-                        temperature[val[0]].append(val[4])
-    print(at_waypoint)
+    #exception handling
+    if num_waypoints == 0:
+        print("Number of waypoints must be higher than 0")
+        exit(1)
+
+    # constants
+    FEET_TO_METERS = 0.305
+    UPPER_LIMIT = 1000
+    LOWER_LIMIT = 50
+
+    # getti
+    #res = []
+    #at_waypoint = []
+
+
+
+    # putting data in lists
+    heights = {}
+    u_comp = {}
+    v_comp = {}
+    temperature = {}
+
+    for en in res:
+        for val in en:
+            if LOWER_LIMIT <= val[0] <= UPPER_LIMIT and val[1] == 'isobaricInhPa':
+                if val[2] == 'Geopotential Height':
+                    if val[0] not in heights:
+                        heights[val[0]] = []
+                    heights[val[0]].append(val[4])
+                elif val[2] == 'U component of wind':
+                    if val[0] not in u_comp:
+                        u_comp[val[0]] = []
+                    u_comp[val[0]].append(val[4])
+                elif val[2] == 'V component of wind':
+                    if val[0] not in v_comp:
+                        v_comp[val[0]] = []
+                    v_comp[val[0]].append(val[4])
+                elif val[2] == 'Temperature':
+                    if val[0] not in temperature:
+                        temperature[val[0]] = []
+                    temperature[val[0]].append(val[4])
     fig, ax = plt.subplots()
 
     ax1 = fig.add_subplot(111)
@@ -170,22 +177,11 @@ def plotting(num_waypoints, flight_data, grib_data):
     #    for j in i:
     #        temp_array.append(j)
 
-    ax1.grid() #2
-    ax1.plot(np.arange(1, num_waypoints + 1, step=1), result_pressure_height) #2
-    #plt.plot(np.arange(1, num_waypoints + 1, step=1), pressure_upper)
-    #plt.plot(np.arange(1,num_waypoints + 1, step=1), pressure_lower)
-    ax1.set_xlabel("Waypoints")
-    ax1.set_xticks(np.arange(1,num_waypoints + 1, step=1))
-    ax1.set_xticklabels(lat_lon)
-    ax1.set_ylabel("Pressure")
-    fig.tight_layout()
-    ax2 = plt.gca() #2
-    ax2.invert_yaxis() #2
-
-    #heatmap, x, y = np.histogram2d(np.array(temp_array), np.arange(0, 230, step=1))
 
     #ax1 = plt.imshow(heat_array, alpha=0.65, cmap='plasma')
-    ax1.imshow(heat_array, alpha=0.65, cmap='plasma')
+    im = ax1.imshow(heat_array, cmap='plasma')
+    #cax = fig.add_axes([0.21, 0.95, 0.6, 0.03])  # [left, bottom, width, height]
+    #ax1.colorbar(im, cax=cax, orientation='vertical')
 
     ax2 = ax1.twinx()
     lvl_cnt = 0
@@ -194,14 +190,27 @@ def plotting(num_waypoints, flight_data, grib_data):
         ax2.barbs(np.arange(1, num_waypoints + 1), lvl, np.array(u_comp.get(lvl)), np.array(v_comp.get(lvl)),
                  length=5.5, rasterized=True)
 
+    ax2.grid() #2
+    ax2.plot(np.arange(1, num_waypoints + 1, step=1), result_pressure_height, color='red') #2
+    ax2 = plt.gca() #2
+    ax2.invert_yaxis() #2
+
+    ax2.set_xlabel("Waypoints")
+    ax2.set_xticks(np.arange(1,num_waypoints + 1, step=1))
+    ax2.set_xticklabels(lat_lon)
+    ax2.set_ylabel("Pressure")
+    fig.tight_layout()
+
     plt.show()
 
 
 if __name__ == '__main__':
+
 
     flight_data = Fd.FlightData('2019-05-01_EDDM-EDDH_Aviator.tsv')
 
     #extracting json data
     grib_data = grb.import_from_json("gfs.t00z.pgrb2.0p25.f003.json")
 
-    plotting(10, flight_data, grib_data)
+    res, wp = interpolating(flight_data, grib_data, 10)
+    plotting(10, res, wp)
